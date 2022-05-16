068eb993c539dad774626b28ba512246031270a0
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-16 14:56:51 -0400

    created prop

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <footer 
+        style={{ width: '100%', backgroundColor: 'wheat' }}
+        className="footer">
+            <p>&copy; Stephanie, 2022</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/navbar.css'
+
+function NavBar() {
+    // do something
+    return (
+        <nav className='navbar'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Features</li>
+                <li>Account</li>
+            </ul>
+
+        </nav>
+    )
+}
+
+
+export default NavBar;
